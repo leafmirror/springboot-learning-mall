3387ed3249cc675b654ee8b2898152b40444b44d
ixingjue <ixingjue@qq.com>
2020-10-04 09:54:56 +0800

    mall整合SpringTask实现定时任务

---
+++ src/main/java/com/leafmirror/malllearning/config/SpringTaskConfig.java
@@ -0,0 +1,14 @@
+package com.leafmirror.malllearning.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * http://www.macrozheng.com/#/architect/mall_arch_06
+ * 定时任务配置
+ */
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+
+}
